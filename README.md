5513aa2bf376980c19b0272c070c11527b1654c1
freewind <nowindlee@gmail.com>
2018-07-28 12:43:45 +0800

    demo

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-Kotlin Hello World Demo
-=======================
+Kotlin Define a Delegate Property without Base Class Demo
+=========================================================
 
-Hello world from kotlin. Open it in IDE and run `Hello.kt`
+The delegate property doesn't need to inherit any base class/interface, all it needs is having such one(or two) methods:
+
+```
+operator fun getValue(...)
+operator fun setValue(...)
+```
+
+See `Hello.kt`.
--- src/main/kotlin/example/Hello.kt
+++ src/main/kotlin/example/Hello.kt
@@ -1,7 +1,25 @@
 package example
 
+import kotlin.reflect.KProperty
+
 fun main(args: Array<String>) {
-    println(hello("Kotlin"))
+    val user = User()
+    user.name = "Delegate"
+    println(user.name)
 }
 
-fun hello(name: String): String = "Hello, $name!"
+class User {
+    var name by hello("Kotlin")
+}
+
+class hello(initValue: String) {
+    private var value = initValue
+
+    operator fun getValue(thisRef: User, property: KProperty<*>): String {
+        return "Hello, $value!"
+    }
+
+    operator fun setValue(thisRef: User, property: KProperty<*>, newValue: String) {
+        value = newValue
+    }
+}
